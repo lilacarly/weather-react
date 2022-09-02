e6d835d749704a26c532d38a722a7c2a9f616e7f
lilacarly <108032321+lilacarly@users.noreply.github.com>
2022-09-01 20:11:01 -0400

    worked on api integration

--- src/Weather.js
+++ src/Weather.js
@@ -1,68 +1,93 @@
-import React from "react";
+import React, { useState } from "react";
 import "./styles.css";
+import axios from "axios";
 export default function Weather() {
-  return (
-    <div className="Weather">
-      <div className="container">
-        <h1>🌦 What's the weather?</h1>
-        <br />
-        <form id="search-form">
-          <input
-            type="text"
-            placeholder="Enter city name here..."
-            className="entry"
-            id="cityEntry"
-          />
-          <input type="submit" value="Search" className="search" />
-        </form>
-        <br />
-        <div className="card">
-          <div className="card-header" id="todayDate"></div>
-          <div className="card-body">
-            <div className="row">
-              <div className="col-4">
-                <p className="card-title city" id="cardCity">
-                  Boston, MA
-                </p>
-                <p className="card-text temp">
-                  <span id="theTemp">85</span>
-                  <button className="units active" id="fahrenheit">
-                    ˚F
-                  </button>
-                  <button className="units" id="fahrenheit">
-                    ˚C
-                  </button>
-                </p>
+  const [ready, setReady] = useState(false);
+  const [weatherData, setWeatherData] = useState({});
+  function handleResponse(response) {
+    console.log(response.data.main.temp);
+    setReady(true);
+    setWeatherData({
+      temperature: Math.round(response.data.main.temp),
+      wind: Math.round(response.data.wind.speed),
+      city: response.data.name,
+      humidity: response.data.main.humidity,
+      description: response.data.weather[0].description,
+    });
+  }
+
+  if (ready) {
+    return (
+      <div className="Weather">
+        <div className="container">
+          <h1>🌦 What's the weather?</h1>
+          <br />
+          <form id="search-form">
+            <input
+              type="text"
+              placeholder="Enter city name here..."
+              className="entry"
+              id="cityEntry"
+            />
+            <input type="submit" value="Search" className="search" />
+          </form>
+          <br />
+          <div className="card">
+            <div className="card-header" id="todayDate"></div>
+            <div className="card-body">
+              <div className="row">
+                <div className="col-4">
+                  <p className="card-title city" id="cardCity">
+                    {weatherData.city}
+                  </p>
+                  <p className="card-text temp">
+                    <span id="theTemp">{weatherData.temperature}</span>
+                    <button className="units active" id="fahrenheit">
+                      ˚F
+                    </button>
+                    <button className="units" id="fahrenheit">
+                      ˚C
+                    </button>
+                  </p>
+                </div>
+                <img src="" alt="Clear" id="emoji" className="col-4" />
+                <div className="col-4">
+                  <ul>
+                    <li id="descriptionElement">{weatherData.description}</li>
+                    <li>
+                      Wind:{" "}
+                      <span id="windspeedElement">{weatherData.wind}</span> mph
+                    </li>
+                    <li>
+                      Humidity:{" "}
+                      <span id="humidityElement">{weatherData.humidity}</span>%
+                    </li>
+                  </ul>
+                </div>
               </div>
-              <img src="" alt="Clear" id="emoji" className="col-4" />
-              <div className="col-4">
-                <ul>
-                  <li id="descriptionElement">Description</li>
-                  <li>
-                    Wind: <span id="windspeedElement"></span> mph
-                  </li>
-                  <li>
-                    Humidity: <span id="humidityElement"></span>%
-                  </li>
-                </ul>
-              </div>
+              <div className="forecast" id="forecast"></div>
             </div>
-            <div className="forecast" id="forecast"></div>
+
+            <script
+              src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/js/bootstrap.bundle.min.js"
+              integrity="sha384-pprn3073KE6tl6bjs2QrFaJGz5/SUsLqktiwsUTF55Jfv3qYSDhgCecCxMW52nD2"
+              crossorigin="anonymous"
+            ></script>
+
+            <script src="script.js"></script>
           </div>
-
-          <script
-            src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/js/bootstrap.bundle.min.js"
-            integrity="sha384-pprn3073KE6tl6bjs2QrFaJGz5/SUsLqktiwsUTF55Jfv3qYSDhgCecCxMW52nD2"
-            crossorigin="anonymous"
-          ></script>
-
-          <script src="script.js"></script>
+          <footer>
+            By Lila Graham,{" "}
+            <a href="https://github.com/lilacarly/weather-react">GitHub</a>
+          </footer>
         </div>
-        <footer>
-          By Lila Graham,{" "}
-          <a href="https://github.com/lilacarly/weather-react">GitHub</a>
-        </footer>
       </div>
-    </div>
-  );
+    );
+  } else {
+    let apiKey = "79556a453ffc6ed66cacb6d61dc994cc";
+    let city = "Boston";
+    let apiUrl = `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=imperial`;
+    axios.get(apiUrl).then(handleResponse);
+    return "Loading...";
+  }
 }
