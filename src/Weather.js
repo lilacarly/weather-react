f33d79f911f692941f043cf4371c41a5c0cda011
lilacarly <108032321+lilacarly@users.noreply.github.com>
2022-09-04 12:40:00 -0400

    updated states

--- src/Weather.js
+++ src/Weather.js
@@ -2,21 +2,22 @@ import React, { useState } from "react";
 import "./styles.css";
 import axios from "axios";
 export default function Weather() {
-  const [ready, setReady] = useState(false);
-  const [weatherData, setWeatherData] = useState({});
+  const [weatherData, setWeatherData] = useState({ ready: false });
   function handleResponse(response) {
     console.log(response.data.main.temp);
-    setReady(true);
+
     setWeatherData({
+      ready: true,
       temperature: Math.round(response.data.main.temp),
       wind: Math.round(response.data.wind.speed),
       city: response.data.name,
       humidity: response.data.main.humidity,
       description: response.data.weather[0].description,
+      iconUrl: "",
     });
   }
 
-  if (ready) {
+  if (weatherData.ready) {
     return (
       <div className="Weather">
         <div className="container">
@@ -50,7 +51,12 @@ export default function Weather() {
                     </button>
                   </p>
                 </div>
-                <img src="" alt="Clear" id="emoji" className="col-4" />
+                <img
+                  src={weatherData.iconURL}
+                  alt={weatherData.description}
+                  id="emoji"
+                  className="col-4"
+                />
                 <div className="col-4">
                   <ul>
                     <li id="descriptionElement">{weatherData.description}</li>
@@ -71,7 +77,7 @@ export default function Weather() {
             <script
               src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/js/bootstrap.bundle.min.js"
               integrity="sha384-pprn3073KE6tl6bjs2QrFaJGz5/SUsLqktiwsUTF55Jfv3qYSDhgCecCxMW52nD2"
-              crossorigin="anonymous"
+              crossOrigin="anonymous"
             ></script>
 
             <script src="script.js"></script>
