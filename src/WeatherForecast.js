1a40bd9fe6ca29191b275b61610d7bb430782c2e
lilacarly <108032321+lilacarly@users.noreply.github.com>
2022-09-11 16:30:34 -0400

    forecast now updates on search

--- src/WeatherForecast.js
+++ src/WeatherForecast.js
@@ -1,12 +1,18 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 
 import "./WeatherForecast.css";
 import WeatherForecastDay from "./WeatherForecastDay";
 import axios from "axios";
+import { cleanup } from "@testing-library/react";
 
 export default function WeatherForecast(props) {
   let [loaded, setLoaded] = useState(false);
   let [forecast, setForecast] = useState(null);
+
+  useEffect(() => {
+    setLoaded(false);
+  }, [props.coordinates]);
+
   function handleResponse(response) {
     setForecast(response.data.daily);
     setLoaded(true);
