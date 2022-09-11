433cc3ed18eee85e659e7bbcf5c8c86cf642dd71
lilacarly <108032321+lilacarly@users.noreply.github.com>
2022-09-11 19:29:38 -0400

    took out cleanup line

--- src/WeatherForecast.js
+++ src/WeatherForecast.js
@@ -4,7 +4,6 @@ import "./styles.css";
 import "./WeatherForecast.css";
 import WeatherForecastDay from "./WeatherForecastDay";
 import axios from "axios";
-import { cleanup } from "@testing-library/react";
 
 export default function WeatherForecast(props) {
   let [loaded, setLoaded] = useState(false);
